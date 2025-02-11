9be690db562314a4cda1282a9791c7f7fbd22c7b
Yovanne05 <yovanne.ponnou@gmail.com>
2025-02-11 16:18:34 +0100

    init getIcon

--- lib/viewmodels/GameViewModel.dart
+++ lib/viewmodels/GameViewModel.dart
@@ -30,8 +30,35 @@ class GameViewModel extends ChangeNotifier{
     notifyListeners();
   }
 
-  void getIcon(){
-
+  Image getIcon(String iconName){
+    switch(iconName){
+      case("bomb"):
+        return Image.asset("assets/bomb.png");
+      case("case0"):
+        return Image.asset("assets/case0.png");
+      case("case1"):
+        return Image.asset("assets/case1.png");
+      case("case2"):
+        return Image.asset("assets/case2.png");
+      case("case3"):
+        return Image.asset("assets/case3.png");
+      case("case4"):
+        return Image.asset("assets/case4.png");
+      case("case5"):
+        return Image.asset("assets/case5.png");
+      case("case6"):
+        return Image.asset("assets/case6.png");
+      case("case7"):
+        return Image.asset("assets/case7.png");
+      case("case8"):
+        return Image.asset("assets/case8.png");
+      case("eplosion"):
+        return Image.asset("assets/explosion.png");
+      case("flag"):
+        return Image.asset("assets/flag.jpg");
+      default:
+        return Image.asset("assets/case0.png");
+    }
   }
 
 }
