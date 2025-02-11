3919b2b3a5366435f6db3cb4b6ef100bd5dc65af
Yovanne05 <yovanne.ponnou@gmail.com>
2025-02-11 14:01:40 +0100

    initialisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:counter_app/viewmodels/CounterViewModel.dart';
 import 'package:flutter/material.dart';
-import 'package:counter_app/views/HomeView.dart';
-import 'package:provider/provider.dart';
 
 const Color primaryColor = Color(0xFF2196F3);
-const String appTitle = 'Compteur App';
-const String secondPageTitle = 'Seconde page';
+const String appTitle = 'Démineur';
 
 void main() {
   runApp(const MyApp());
@@ -16,23 +12,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (_) => CounterViewModel(),
-      child: MaterialApp(
-        title: appTitle,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
-          useMaterial3: true,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const HomePage(),
-          '/second': (context) => const SecondPage(),
-        },
+    return MaterialApp(
+      title: appTitle,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+        useMaterial3: true,
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+      },
     );
   }
 }
+
 
 /*
  * Page d'accueil et son état mutable
@@ -44,10 +37,6 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-
-/*
- * Etat mutable de la page d'accueil
- */
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
@@ -56,42 +45,11 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Row(
             children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacementNamed(context, '/');
-                },
-                child: Image.asset('assets/logo.png', height: 40),
-              ),
-              const SizedBox(width: 10),
               const Text(appTitle),
             ],
           )
       ),
-      body: Center(child : HomeView()),
     );
 
-  }
-}
-
-/*
- * La seconde page de votre app
- */
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(secondPageTitle),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: const Center(
-        child: Text(
-          'Bienvenue sur la seconde page !',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
   }
 }
